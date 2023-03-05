8f50af59bb2cbc5bfcbc3f3d7bf03d137a1a7e2f
Sudhakar Swain <91896763+sudhakar-swain-au27@users.noreply.github.com>
2023-03-05 13:12:43 +0530

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { ScatterChart,Scatter, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import wineData from './wineData.json';
 
-function App() {
+
+interface WineData {
+  alcohol: number;
+  color_intensity: number;
+  magnesium: number;
+  wine_type: string;
+  
+}
+
+
+const App = () => {
+  const [data, setData] = useState<WineData[]>([]);
+
+  useEffect(() => {
+    setData(wineData);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Wine Data Visualization</h1>
+      <h2>Scatter Plot: Color Intensity and Hue </h2>
+      <ScatterChart width={500} height={400}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis type="number" dataKey="Color intensity" name="Color intensity" unit="%" />
+        <YAxis type="number" dataKey="Hue" name="Hue" unit="units" />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Legend />
+        <Scatter name="Wine Data" data={data} fill="#8884d8" />
+      </ScatterChart>
+      <h2>Bar Chart: Alcohol and Malic Acid</h2>
+      <BarChart width={500} height={400} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="Alcohol" />
+        <YAxis dataKey="Malic Acid" name="Malic Acid" unit="mg/L" />
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+        <Legend />
+        <Bar dataKey="Malic Acid" fill="#8884d8" name="Malic Acid" />
+      </BarChart>
     </div>
   );
-}
+};
 
 export default App;
